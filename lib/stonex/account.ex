df6a8e26ef831b051535a0de979baa6b53ee7b37
Lucas Stellet <lustepe@gmail.com>
2021-07-21 19:57:08 -0300

    feat: create Account schema

---
+++ lib/stonex/account.ex
@@ -0,0 +1,34 @@
+defmodule Stonex.Account do
+  @moduledoc """
+  Creates a new bank account.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+
+  schema "accounts" do
+    field :balance, :decimal, precision: 10, scale: 2, default: 1000
+    field :number, :string
+    field :branch, :string, default: "0001"
+    field :digit, :string
+
+    belongs_to :user, Stonex.User
+
+    timestamps()
+  end
+
+  @spec build(map) :: {:ok, %Ecto.Changeset{}} | {:error, %Ecto.Changeset{}}
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  @spec changeset(map) :: %Ecto.Changeset{}
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, [:number, :digit])
+  end
+end
