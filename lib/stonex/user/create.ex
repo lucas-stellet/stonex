86cb2c2cbe8b0165048dc7b1e648d8e83007dee3
Lucas Stellet <lustepe@gmail.com>
2021-07-22 20:23:35 -0300

    feat: create logic to avoid create repeated user

--- lib/stonex/user/create.ex
+++ lib/stonex/user/create.ex
@@ -8,11 +8,32 @@ defmodule Stonex.User.Create do
   alias Stonex.Repo
 
   def call(params) do
-    params
-    |> User.build()
-    |> create_user()
+    case find_user(params) do
+      :user_not_exist ->
+        params
+        |> User.build()
+        |> create_user()
+
+      :user_already_exists ->
+        {:error, "User already exists!"}
+    end
   end
 
   defp create_user({:ok, changeset}), do: Repo.insert(changeset)
   defp create_user({:error, _changeset} = error), do: error
+
+  defp find_user(params) do
+    case Stonex.get_user(convert_params(params)) do
+      {:error, "User not found"} ->
+        :user_not_exist
+
+      {:ok, _user} ->
+        :user_already_exists
+    end
+  end
+
+  def convert_params(map) do
+    Enum.map(map, fn {key, value} -> {String.to_existing_atom(key), value} end)
+    |> Keyword.delete(:password)
+  end
 end
