dc6cd826690d32b0bcd775f293a79bda144f3517
Lucas Stellet <lustepe@gmail.com>
2021-07-22 17:44:05 -0300

    feat: remove creation of number and digit account logic

--- lib/stonex/account/create.ex
+++ lib/stonex/account/create.ex
@@ -7,20 +7,12 @@ defmodule Stonex.Account.Create do
   alias Stonex.Account
   alias Stonex.Repo
 
-  def call(%{user_id: user_id}) do
-    account_body = generate_number_digit_account()
-
-    Map.put(account_body, :user_id, user_id)
+  def call(params) do
+    params
     |> Account.build()
     |> create_account()
   end
 
   defp create_account({:ok, changeset}), do: Repo.insert(changeset)
   defp create_account({:error, _changeset} = error), do: error
-
-  defp generate_number_digit_account do
-    %{}
-    |> Map.put(:number, Integer.to_string(Enum.random(100_000..999_999)))
-    |> Map.put(:digit, Integer.to_string(Enum.random(1..9)))
-  end
 end
