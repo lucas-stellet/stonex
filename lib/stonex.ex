b30b4f7551134d006eca740c7db65d4bcc05847d
Lucas Stellet <lustepe@gmail.com>
2021-07-22 16:54:51 -0300

    feat: delegate facade modules to Stonex domain functions

--- lib/stonex.ex
+++ lib/stonex.ex
@@ -6,4 +6,10 @@ defmodule Stonex do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias Stonex.{Account, User}
+
+  defdelegate create_account(params), to: Account.Create, as: :call
+
+  defdelegate create_user(params), to: User.Create, as: :call
 end
