7c61837a6a21b1230b4710aea84740d708daf28d
Lucas Stellet <lustepe@gmail.com>
2021-07-22 17:36:05 -0300

    feat: create accounts view and create function view

---
+++ lib/stonex_web/views/accounts_view.ex
@@ -0,0 +1,27 @@
+defmodule StonexWeb.AccountView do
+  use StonexWeb, :view
+
+  def render("create.json", %{
+        account: %{
+          balance: balance,
+          number: number,
+          branch: branch,
+          digit: digit,
+          user: %{
+            first_name: first_name,
+            last_name: last_name
+          }
+        }
+      }) do
+    %{
+      message: "Account created!",
+      account: %{
+        number: number,
+        branch: branch,
+        digit: digit,
+        balance: balance,
+        account_owner: first_name <> " " <> last_name
+      }
+    }
+  end
+end
