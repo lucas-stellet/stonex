6be7f95aed13f27139a896ba5cba6cf333d9f6c8
Lucas Stellet <lustepe@gmail.com>
2021-07-22 20:33:02 -0300

    chore: add guardian library

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Stonex.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
