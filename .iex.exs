8520b42f11af16497e56b5f5d42f07690ff6821b
Lucas Stellet <lustepe@gmail.com>
2021-07-23 16:31:53 -0300

    chore: add new alias to to iex script

--- .iex.exs
+++ .iex.exs
@@ -1,2 +1,6 @@
-alias Stonex.{Account, User}
+alias Stonex.{Account, User, Transaction}
 alias Stonex.Repo
+
+alias Stonex.Account.Create, as: AccountCreate
+alias Stonex.User.Create, as: UserCreate
+alias Stonex.Transaction.Transfer.Create, as: TransferCreate
