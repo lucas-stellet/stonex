20361c0ff20a7c9b350f3c83c57cca0b1832a913
Lucas Stellet <lustepe@gmail.com>
2021-07-26 18:44:10 -0300

    feat: create delegation functions
    functions as do transfer, get reports, and manage accounts

--- lib/stonex.ex
+++ lib/stonex.ex
@@ -7,9 +7,18 @@ defmodule Stonex do
   if it comes from the database, an external API or others.
   """
 
-  alias Stonex.{Account, User}
+  alias Stonex.{Account, User, Transaction, Backoffice}
 
   defdelegate create_account(params), to: Account.Create, as: :call
+  defdelegate get_account(params), to: Account.Get, as: :call
+  defdelegate update_account(params), to: Account.Update, as: :call
 
   defdelegate create_user(params), to: User.Create, as: :call
+  defdelegate get_user(params), to: User.Get, as: :call
+  defdelegate get_user_by_document(params), to: User.GetByDocument, as: :call
+
+  defdelegate do_transfer(params), to: Transaction.Transfer.Create, as: :call
+  defdelegate do_withdraw(params), to: Transaction.Withdraw.Create, as: :call
+
+  defdelegate get_reports(params), to: Backoffice.Reports, as: :call
 end
