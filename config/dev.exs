b3960fa9de5196f4e9b3e6c01db6ec75ce608eb4
Lucas Stellet <lustepe@gmail.com>
2021-07-26 23:42:00 -0300

    chore: change the database hostname to work with docker

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :stonex, Stonex.Repo,
   username: "postgres",
   password: "postgres",
   database: "stonex_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
